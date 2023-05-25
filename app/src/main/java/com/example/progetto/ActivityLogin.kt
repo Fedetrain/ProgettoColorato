4352a28699e19befdbf317d2ac81d21f4047e16f
AlexOwna <alexowna1201@gmail.com>
2023-05-25 17:49:40 +0200

    cicici

--- app/src/main/java/com/example/progetto/ActivityLogin.kt
+++ app/src/main/java/com/example/progetto/ActivityLogin.kt
@@ -14,6 +14,8 @@ class ActivityLogin : AppCompatActivity() {
     private lateinit var binding:ActivityLoginBinding
     private lateinit var dbManager: DBManager
 
+    //ciaoap
+
     @SuppressLint("Range")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
